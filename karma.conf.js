e3383850913f0176a6f65a0b7d5ef1c127a88892
Jean Honlet <marielineet.jean@gmail.com>
2018-06-05 20:24:51 +0200

    Empty project but working

---
+++ karma.conf.js
@@ -0,0 +1,61 @@
+/* eslint-env node */
+/* eslint no-console: off */
+
+module.exports = function(config) {
+	var configuration = {
+		plugins : [
+			'karma-chrome-launcher',
+			// 'karma-firefox-launcher',
+			'karma-jasmine',
+			'karma-coverage',
+			'karma-html-reporter'
+		],
+
+		frameworks : [
+			'jasmine',
+		],
+
+		reporters : [
+			'progress',
+			'coverage',
+			'html'
+		],
+
+		files : [
+			{ pattern: 'node_modules/**',                  included: false, served: true, watched: false },
+			'tests/*.js',
+			'withElement.js'
+		],
+
+		autoWatch : true,
+
+		browsers: [
+			// 'FirefoxHeadless',
+			'ChromeHeadless'
+		],
+
+		// // https://github.com/karma-runner/karma-firefox-launcher/issues/76
+		// customLaunchers: {
+		// 	FirefoxHeadless: {
+		// 		base: 'Firefox',
+		// 		flags: [ '-headless' ],
+		// 	},
+		// },
+
+		preprocessors: {
+			'static/**/*.js': [ 'coverage' ],
+		},
+
+		coverageReporter: {
+			type :  'lcov',
+			dir :   __dirname + '/',
+			subdir: 'target/'
+		},
+
+		htmlReporter: {
+			outputDir: __dirname + '/target/html/',
+		},
+	};
+
+	config.set(configuration);
+};
---
+++ withElement.js
@@ -0,0 +1,22 @@
+
+// window.jasmine = jasmineRequire.core(jasmineRequire);
+// jasmineRequire.html(jasmine);
+// var env = jasmine.getEnv();
+// var jasmineInterface = jasmineRequire.interface(jasmine, env);
+// jasmineInterface.before = jasmineInterface.beforeEach;
+// jasmineInterface.after = jasmineInterface.afterEach;
+// jasmineInterface.context = jasmineInterface.describe;
+
+// waits(0);
+// beforeEach(function(){
+// 	waitsFor(function(){
+// 	  if (typeof(CustomElements) == 'undefined') return false;
+// 	  return CustomElements.ready;
+// 	});
+// });
+
+// if (typeof window == "undefined" && typeof exports == "object") {
+//   extend(exports, jasmineInterface);
+// } else {
+//   extend(window, jasmineInterface);
+// }
