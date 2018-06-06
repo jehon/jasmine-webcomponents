d38ba6707f4bad6d4c0950a3406b460dfa8c9589
Jean Honlet <marielineet.jean@gmail.com>
2018-06-06 20:31:44 +0200

    Add comments

--- tests/test.js
+++ tests/test.js
@@ -13,12 +13,14 @@ class TestElement extends HTMLElement {
 }
 window.customElements.define('test-element', TestElement);
 
+/* Check that karma is still ok */
 describe('root test', function() {
 	it('works', function() {
 		expect(true).toBeTruthy();
 	});
 });
 
+/* The real test for div component */
 withElement('<div></div>', function(element) {
 	it('should work', function() {
 		expect(element()).not.toBe(null);
