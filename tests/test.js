a4f5999d8399966d452707fd2f754933beacf146
Jean Honlet <marielineet.jean@gmail.com>
2018-06-07 22:23:16 +0200

    Add some options

--- tests/test.js
+++ tests/test.js
@@ -5,10 +5,24 @@ class TestElement extends HTMLElement {
 	constructor() {
 		super();
 		this.test = 0;
+		this.intervalCount = 0;
+		this.interval = setInterval(() => {
+			this.intervalCount++;
+			if (this.intervalCount > 10) {
+				this.clear();
+			}
+		}, 10);
 	}
 
 	incr() {
 		this.test++;
+	}
+
+	clear() {
+		if (this.interval) {
+			clearInterval(this.interval);
+		}
+		this.interval = false;
 	}
 }
 window.customElements.define('test-element', TestElement);
@@ -21,9 +35,106 @@ describe('root test', function() {
 });
 
 /* The real test for div component */
-withElement('<div></div>', function(element) {
-	it('should work', function() {
-		expect(element()).not.toBe(null);
-		expect(element().tagName).toBe('DIV');
+describe('in text mode', function() {
+	withHtml('<div></div>', function(element) {
+		it('should work', function() {
+			expect(element().tagName).toBe('DIV');
+		});
+	});
+});
+
+describe('with multiple elements', function() {
+	withHtml({
+		html: '<div></div><span></span>',
+		assertElementIsNotNull: true
+	}, function(element) {
+		it('should work', function() {
+			expect(element().length).toBe(2);
+			expect(element()[0].tagName).toBe('DIV');
+			expect(element()[1].tagName).toBe('SPAN');
+		});
+	});
+});
+
+describe('in option mode', function() {
+	withHtml({
+		title: 'test with title',
+		html: '<test-element></test-element>'
+	}, function(element) {
+		it('should work', function() {
+			expect(element().tagName).toBe('TEST-ELEMENT');
+			expect(element().incr).toEqual(jasmine.any(Function));
+		});
+	});
+
+	withHtml({
+		title: 'test with forceList',
+		html: '<test-element></test-element>',
+		forceList: true
+	}, function(element) {
+		it('should work', function() {
+			expect(element()).toEqual(jasmine.any(NodeList));
+			expect(element().length).toBe(1);
+			expect(element()[0].tagName).toBe('TEST-ELEMENT');
+		});
+	});
+
+	withHtml({
+		title: 'test with title',
+		html: '<test-element></test-element>',
+		assertElementIsNotNull: true
+	}, function(element) {
+		it('should work', function() {
+			expect(element().tagName).toBe('TEST-ELEMENT');
+		});
+	});
+
+	withHtml({
+		title: 'test with setupTime',
+		html: '<test-element></test-element>',
+		setupTime: 100
+	}, function(element) {
+		it('should work', function() {
+			expect(element().tagName).toBe('TEST-ELEMENT');
+			expect(element().intervalCount).toBeGreaterThan(9);
+		});
+	});
+
+	describe('test beforeAll', function() {
+		withHtml({
+			title: 'test with setupTime',
+			html: '<test-element></test-element>',
+			// beforeEach: false < default value >
+		}, function(element) {
+			it('should work', function() {
+				expect(element().tagName).toBe('TEST-ELEMENT');
+				element().incr();
+				expect(element().test).toBe(1);
+			});
+
+			it('should continue with the same object', function() {
+				expect(element().tagName).toBe('TEST-ELEMENT');
+				expect(element().test).toBe(1);
+			});
+		});
+	});
+
+	describe('test beforeEach', function() {
+		withHtml({
+			title: 'test with setupTime',
+			html: '<test-element></test-element>',
+			beforeEach: true
+		}, function(element) {
+			it('should work', function() {
+				expect(element().tagName).toBe('TEST-ELEMENT');
+				element().incr();
+				expect(element().test).toBe(1);
+			});
+
+			it('should continue with the same object', function() {
+				expect(element().tagName).toBe('TEST-ELEMENT');
+				expect(element().test).toBe(0);
+			});
+		});
 	});
 });
